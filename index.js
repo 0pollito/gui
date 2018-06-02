fa2245ce048ecb5155b095ae6065ece75bb4fb1f
Espinoza Isna <zerospin@hotmail.com>
2018-06-02 16:15:24 -0500

    buttonListener

--- index.js
+++ index.js
@@ -6,28 +6,35 @@ var Gpio = require('pigpio').Gpio,
 var say = require('say');
  
  var distancia = 0;
+
 // Level must be stable for 50 ms before an alert event is emitted.
 button.glitchFilter(50000);
 
-setTimeout(function(){ say('Iniciando Guiame'); }, 3000);
-
-
-button.on('alert', (level, tick) => {
-  if (level === 0) {
-    if(count==0){
-      count++;
-      console.log('Modo Preventivo :: '+count);
-      setTimeout(function(){ say.speak('Modo proventivo Iniciado'); }, 3000);
-      
-      sensar();
-    }else{
-      count--;
-      setTimeout(function(){ say.speak('Cambiando a modo Reconocimiento'); }, 3000);
+let inicio = new Promise((resolve, reject) => {
+  say.speak('Iniciando Asistente Guíame');
+  resolve("¡Guíame Iniciada!");
+}).then( (successMessage)=> {
+  //Init button listener
+  button.on('alert', (level, tick) => {
+    if (level === 0) {
+      if(count==0){
+        count++;
+        console.log('Modo Preventivo :: '+count);
+        say.speak('Modo Preventivo Iniciado');
+        sensar();
+      }else{
+        count--;
+        console.log('Modo Reconocimiento :: '+count);
+        trigger.digitalWrite(0);
+        say.speak('Modo Reconocimiento iniciado');
+      }
     }
-  }
+  });
 });
 
 
+
+//function to detect obstacles
 function sensar(){
   // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
   var MICROSECDONDS_PER_CM = 1e6/34321;
@@ -46,7 +53,7 @@ function sensar(){
         diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
         distancia = diff / 2 / MICROSECDONDS_PER_CM;
         if(distancia < 20){
-          setTimeout(function(){ say.speak('Cuidado, obstaculo detectado'); }, 5000);
+          say.speak('Cuidado, obstaculo detectado');
           console.log('Objeto detectado :: '+distancia);
         }
       }
@@ -54,6 +61,6 @@ function sensar(){
   }());
   // Trigger a distance measurement once per two second
   setInterval(function () {
-    trigger.trigger(20, 1); // Set trigger high for 10 microseconds
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
   }, 2000);
 }
