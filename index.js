e22e916b454cd53d1825fae21fb670cfa8ea5080
Espinoza Isna <zerospin@hotmail.com>
2018-06-02 15:15:31 -0500

    timeout add

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ var say = require('say');
  var distancia = 0;
 // Level must be stable for 50 ms before an alert event is emitted.
 button.glitchFilter(50000);
- 
+
+setTimeout(function(){ say('Iniciando Guiame'); }, 3000);
+
+
 button.on('alert', (level, tick) => {
   if (level === 0) {
     if(count==0){
       count++;
       console.log('Modo Preventivo :: '+count);
-      say.speak('Modo proventivo Iniciado');
+      setTimeout(function(){ say.speak('Modo proventivo Iniciado'); }, 3000);
+      
       sensar();
     }else{
       count--;
-      say.speak('Modo Reconocimiento Iniciado');
-      console.log('Modo Reconocimiento :: '+count);
+      setTimeout(function(){ say.speak('Cambiando a modo Reconocimiento'); }, 3000);
     }
   }
 });
@@ -33,19 +36,17 @@ function sensar(){
  
   (function () {
     var startTick;
-   
     echo.on('alert', function (level, tick) {
       var endTick,
         diff;
-   
       if (level == 1) {
         startTick = tick;
       } else {
         endTick = tick;
         diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
         distancia = diff / 2 / MICROSECDONDS_PER_CM;
-        if(distancia>20 && distancia<30){
-          say.speak('Cuidado, Objecto detectado');
+        if(distancia < 20){
+          setTimeout(function(){ say.speak('Cuidado, obstaculo detectado'); }, 5000);
           console.log('Objeto detectado :: '+distancia);
         }
       }
