b7d600ac22b6d2e9185e26d4d378f9a47f61a594
Espinoza Isna <zerospin@hotmail.com>
2018-06-21 21:00:15 -0500

    pVz2

--- prueba.js
+++ prueba.js
@@ -1,20 +1,3 @@
-var exec = require('child_process').exec, child;
-var command = 'echo "El miércoles es el cuarto día de la semana." | iconv -f utf-8 -t iso-8859-1 | festival --tts';
-child = exec(command, function(error,stdout,stderr) {
-  console.log(stdout);
-  if(error != null){
-    console.log('exec error: ' + error);
-  }
-})
+var player = require('./lib/audio');
 
-
-/*
-checar 
-const clipboardy = require('clipboardy');
-
-// Copy
-clipboardy.writeSync('akjsdhka');
-
-// Paste
-clipboardy.readSync('akjshdka');
- */
+player.play("tecnología, mucha, pero mucha tecnología");
