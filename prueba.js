838c5b5864cea4659970e3f987b74e3a7f4d5e2b
Espinoza Isna <zerospin@hotmail.com>
2018-06-21 21:23:47 -0500

    pVz3

--- prueba.js
+++ prueba.js
@@ -1,3 +1,4 @@
 var player = require('./lib/audio');
 
-player.play("tecnología, mucha, pero mucha tecnología");
+player.play("tecnología, mucha, pero mucha tecnología")
+  .then(() -> player.play("tecnología, mucha, pero mucha tecnología"));
