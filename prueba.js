02c675233731827a40d9a3c9684979ec8012f3f5
Espinoza Isna <zerospin@hotmail.com>
2018-06-21 23:51:47 -0500

    foto

--- prueba.js
+++ prueba.js
@@ -1,9 +1,33 @@
 var player = require('./lib/audio');
 
-player.play("primera línea")
-  .then(function (){
-    return player.play("segunda línea de texto")
-  })
-  .then(function () {
-    return player.play("ultima línea reproduciendo")
+function internet(){
+  return new Promise(function(resolve,reject){
+     require('dns').resolve('www.google.com',function(err){
+      if(err){
+        player.play('sin conexión a internet')
+          .then(function(){
+            reject();
+          });
+      }
+      resolve();
+     });
   });
+}
+
+  player.play('Modo Reconocimiento')
+    .then(function() {
+      return internet()
+    })
+    .then(function(){
+      return camera.photo()
+    })
+    .then(function() {
+      return image.sendImage()
+        .then(function(respuesta){
+          console.log(respuesta);
+          return player.play(respuesta);
+        })
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
